a9a3b85a14ebff65bd5b1db154f20e9868bb71cc
LeonardoRomulo <leo.romulo15@gmail.com>
2025-03-30 08:43:33 -0300

    feat: adição do hero na página home e a importação e utilização da fonte poppin

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,19 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import banner from "../../public/banner-home.png"
+import estilos from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      
+    <div className={estilos.page}>
+      <main>
+        <div className={estilos.banner}>
+          <Image className={estilos.img_banner} src={banner} alt="banner"></Image>
+        </div>
+        <div className={estilos.container}>
+          <h1>Sorverte Artesanal</h1>
+        </div>
+
+      </main>
     </div>
   );
 }
